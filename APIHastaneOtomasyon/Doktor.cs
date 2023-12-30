40beedfa349855117292c2a7d87d77d54b92f2e2
senasal <aksusenaa1@gmail.com>
2023-12-31 01:27:19 +0300

    api eklendi

---
+++ APIHastaneOtomasyon/Doktor.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIHastaneOtomasyon.Models
+{
+    public class Doktor
+    {
+
+        [Key]
+        public int Id { get; set; } 
+
+        [Required(ErrorMessage = "Boş geçmeyiniz")]
+        public string doktoradi { get; set; }
+
+       
+        
+        [Required(ErrorMessage ="Admin bunu boş geçme")]        
+        public int muayeneucreti { get; set; }
+
+        [ForeignKey("Poliklinik")]
+        public int PoliklinikId { get; set; }
+        public Poliklinik poliklinik { get; set; }
+     
+
+      //  virtual public ICollection<Randevu> Randevus { get; set; }  
+
+
+    }
+}
---
+++ APIHastaneOtomasyon/Randevu.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIHastaneOtomasyon.Models
+{
+    public class Randevu
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(11, ErrorMessage = "kullanici adinizi giriniz")]
+        public string tc { get; set; }
+
+        public DateTime randevuZamani { get; set; }
+
+        [ForeignKey("Doktor")]
+
+        public int doktorId { get; set; }
+        public Doktor doktor { get; set; }
+    }
+}
